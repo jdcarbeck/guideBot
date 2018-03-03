e8ebf00f9e97c0a4ebf7c7ced6f439a133c4d487
John Carbeck <jdcarbeck@gmail.com>
2018-03-03 20:45:33 +0000

    Added elements to the chat page and begun trying to make them operate correctly

--- guidanceChatBot/chatInterface/src/app/components/chatpage/chatpage.component.ts
+++ guidanceChatBot/chatInterface/src/app/components/chatpage/chatpage.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-chatpage',
   templateUrl: './chatpage.component.html',
   styleUrls: ['./chatpage.component.css']
 })
 export class ChatpageComponent implements OnInit {
-
+  
+  messages:string[]; 
+  
   constructor() { }
 
   ngOnInit() {
   }
+  
+  messageSend(userMessage: string) {
+    this.messages.push(userMessage);
+    //send message to server here!
+    return 0;
+  }
 
 }
+
+
