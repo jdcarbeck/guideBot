6290f388c4574fc4e29257f71df4bef30d64a3f6
John Carbeck <jdcarbeck@gmail.com>
2018-03-07 18:39:46 +0000

    linked the server to the app component

--- guidanceChatBot/chatInterface/src/app/app.component.ts
+++ guidanceChatBot/chatInterface/src/app/app.component.ts
@@ -10,8 +10,8 @@ export class AppComponent implements OnInit{
   title = 'app';
 
   ngOnInit(): void {
-    //const socket = socketIo('http://localhost:3000');
-    //'hello is the name of the event'
-    //socket.on('hello', (data) => console.log(data));
+    const socket = socketIo('http://localhost:3000');
+    'hello is the name of the event'
+    socket.on('hello', (data) => console.log(data));
   }
 }
