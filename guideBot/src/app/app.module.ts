ffee75620537ec74530dc9c76ea079bc4c5d4096
John Carbeck <jdcarbeck@gmail.com>
2018-03-13 15:17:48 +0000

    chatpage update

--- guideBot/src/app/app.module.ts
+++ guideBot/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
@@ -19,6 +20,7 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     NgbModule.forRoot(),
     RouterModule.forRoot([
       {path: '', component: HomepageComponent},
